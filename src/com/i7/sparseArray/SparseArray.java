aa81c08b93ae8def6ef303e34603cf8c648de864
i78086 <623514624@qq.com>
2019-07-08 17:06:32 +0800

    add method of read from file

--- src/com/i7/sparseArray/SparseArray.java
+++ src/com/i7/sparseArray/SparseArray.java
@@ -1,5 +1,9 @@
 package com.i7.sparseArray;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SparseArray {
 
     //创建一个原始的二维数组
@@ -48,19 +52,93 @@ public class SparseArray {
         }
     }
 
+    //将稀疏数组保存到文件array.txt
+    public void writeToFile(int[][] array) {
+        FileWriter fileWriter = null;
+        File file = new File("Z:/英雄时刻/array.txt");
+        try {
+            fileWriter = new FileWriter(file);
+            for (int i = 0; i < array.length; i ++) {
+                for (int j = 0; j<array[0].length; j++) {
+                    fileWriter.write(array[i][j]+"\t");
+                }
+                fileWriter.write("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //读取文件array.txt并保存入二维数组
+    public int[][] readToArray(String path) {
+        BufferedReader bufferedReader = null;
+        int[][] arry = null;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(new File(path)));
+            List<int[]> list = new ArrayList<int[]>();
+            String s;
+            while ((s = bufferedReader.readLine()) != null) {
+                String[] sArr = s.split("\t");
+                int[] iArr = new int[sArr.length];
+                for (int i = 0; i < sArr.length; i++) {
+                    iArr[i] = Integer.parseInt(sArr[i]);
+                }
+                list.add(iArr);
+            }
+//            System.out.println(list);
+//            for (int[] a : list) {
+//                for (int i : a) {
+//                    System.out.print(i + "\t");
+//                }
+//                System.out.println();
+//            }
+            //获取二维数组的最大列数
+            int maxColum = 0;
+            for (int i = 0 ; i < list.size(); i ++) {
+                if (maxColum < list.get(i).length) {
+                    maxColum = list.get(i).length;
+                }
+            }
+            //初始化二维数组，并将list中的值赋给数组
+            arry = new int[list.size()][maxColum];
+            for (int i = 0 ; i < list.size(); i ++ ) {
+                for (int j = 0; j < maxColum; j++) {
+                    arry[i][j] = list.get(i)[j];
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bufferedReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return arry;
+    }
+
     public static void main(String[] args) {
 
         SparseArray sparseArray = new SparseArray();
-        //创建一个二维数组并赋值
-        int[][] originalArray = sparseArray.creatOriginalArray(11,11);
-        originalArray[1][2] = 1;
-        originalArray[2][3] = 2;
-        originalArray[3][5] = 1;
-        System.out.println("原始数组：");
-        sparseArray.printArray(originalArray);
-        int[][] array = sparseArray.toSparseArray(originalArray);
-        System.out.println("稀疏数组：");
-        sparseArray.printArray(array);
+//        创建一个二维数组并赋值
+//        int[][] originalArray = sparseArray.creatOriginalArray(11,11);
+//        originalArray[1][2] = 1;
+//        originalArray[2][3] = 2;
+//        originalArray[3][5] = 1;
+//        System.out.println("原始数组：");
+//        sparseArray.printArray(originalArray);
+//        int[][] array = sparseArray.toSparseArray(originalArray);
+//        System.out.println("稀疏数组：");
+//        sparseArray.printArray(array);
+//        sparseArray.writeToFile(array);
+//        sparseArray.printArray(sparseArray.readToArray("Z:/英雄时刻/array.txt"));
     }
 }
 
